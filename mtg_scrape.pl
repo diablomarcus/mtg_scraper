32ecc14dc0f62eefdfe901964029cf1042b8005c
diablomarcus <diablomarcus@gmail.com>
2012-03-19 19:03:29 -0500

    Added comments. Fixed URL

--- mtg_scrape.pl
+++ mtg_scrape.pl
@@ -7,7 +7,6 @@ use Web::Scraper;
 
 sub grab_page {
    my $address = $_[0];
-   print($address,"\n");
 };
 
 # This is the URL we're going to scrape for data
@@ -15,14 +14,13 @@ my $url1 = 'http://gatherer.wizards.com/Pages/Search/Default.aspx?page=';
 my $url2 = '&sort=cn+&output=checklist&action=advanced&set=+%5b%22Dark+Ascension%22%5d';
 my $x=0; #For now, we're only using the first page
 
-#Removing for-loop for simplicity during testing of data-gathering
-#for(my $x=0; $x<=1; $x++){
-grab_page("$url1$x$url2");
-#}
+my $address=grab_page("$url1$x$url2");
 
 #instantiate the scraper
 my $cardsData=scraper {
+   #Loop through each row of the checklist page
    process "tr.cardItem", 'cardRow[]' => scraper {
+      #These should be self-explanatary
       process "td.number", number => 'TEXT';
       process "td.name > a.nameLink", name => 'TEXT';
       process "td.artist", artist => 'TEXT';
@@ -32,9 +30,10 @@ my $cardsData=scraper {
 };
 
 #scrape the site
-my $res = $cardsData->scrape(URI->new($url1));
+my $res = $cardsData->scrape(URI->new($address));
 
 #loop through the hits
 for my $i (@{$res->{cardRow}}) {
+   #Print the results
    print "$i->{number} $i->{name} $i->{color} $i->{rarity} by $i->{artist}\n";
 }
