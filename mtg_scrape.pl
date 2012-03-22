908e09573ed94ff3bb657c91510dbf362742eb77
diablomarcus <diablomarcus@gmail.com>
2012-03-21 23:08:28 -0500

    Refactored, renamed vars, added stubs for looping. Changed output location to tmp

--- mtg_scrape.pl
+++ mtg_scrape.pl
@@ -9,15 +9,32 @@ use XML::Writer;
 
 
 #Define our output container
-my $output = IO::File->new(">output.xml");
+my $output = IO::File->new(">/tmp/output.xml");
 #Define our XML writer
 my $writer = XML::Writer->new(OUTPUT=>$output, NEW_LINES=>1,
    DATA_MODE=>1, DATA_INDENT=>1);
+
+sub compact_scraper {
+   my $compactScraper=scraper {
+      #Loop through each row of the checklist page
+      process "tr.cardItem", 'cardRows[]' => scraper {
+         #These should be self-explanatary
+         process "td.number", number => 'TEXT';
+         process "td.name > a.nameLink", name => 'TEXT';
+         process "td.name > a.nameLink", link => '@href';
+         process "td.artist", artist => 'TEXT';
+         process "td.color", color => 'TEXT';
+         process "td.rarity", rarity => 'TEXT';
+      };
+   };
+   return $compactScraper
+};
 
 sub grab_page {
    return $_[0];
 };
 
+#TODO: Build multi-page scrapes
 # This is the URL we're going to scrape for data
 my $url1 = 'http://gatherer.wizards.com/Pages/Search/Default.aspx?page=';
 my $url2 = '&sort=cn+&output=checklist&action=advanced&set=+%5b%22Dark+Ascension%22%5d';
@@ -25,35 +42,23 @@ my $x=0; #For now, we're only using the first page
 
 my $address=grab_page("$url1$x$url2");
 
-#instantiate the scraper
-my $compactScraper=scraper {
-   #Loop through each row of the checklist page
-   process "tr.cardItem", 'cardRows[]' => scraper {
-      #These should be self-explanatary
-      process "td.number", number => 'TEXT';
-      process "td.name > a.nameLink", name => 'TEXT';
-      process "td.name > a.nameLink", link => '@href';
-      process "td.artist", artist => 'TEXT';
-      process "td.color", color => 'TEXT';
-      process "td.rarity", rarity => 'TEXT';
-   };
-};
-
 #scrape the site
-my $res = $compactScraper->scrape(URI->new($address));
+my $results=compact_scraper()->scrape(URI->new($address));
 
-$writer->startTag("DKA");
+$writer->startTag("cards");
 #loop through the hits
-for my $card (@{$res->{cardRows}}) {
-	$writer->startTag($card->{name}); #Open card xml
-	$writer->dataElement('card_link', "$card->{link}");
-	$writer->dataElement('card_number', "$card->{number}");
-	$writer->dataElement('card_rarity', "$card->{rarity}");
-	$writer->dataElement('card_color', "$card->{color}");
-	$writer->dataElement('card_artist', "$card->{artist}");
-	$writer->endTag($card->{name});
+for my $card (@{$results->{cardRows}}) {
+   $writer->startTag($card->{name}); #Open card xml
+
+   #Store elements
+   $writer->dataElement('card_link', "$card->{link}");
+   $writer->dataElement('card_number', "$card->{number}");
+   $writer->dataElement('card_rarity', "$card->{rarity}");
+   $writer->dataElement('card_color', "$card->{color}");
+   $writer->dataElement('card_artist', "$card->{artist}");
+   $writer->endTag($card->{name});
 }
-$writer->endTag("DKA");
+$writer->endTag("cards");
 $writer->end(); #close our writer class
 
 #instantiate the card_detail scraper
@@ -69,6 +74,6 @@ my $cardDetail=scraper {
 
 
 #TODO: loop through the rows we have to ping each link
-for my $card (@{$res->{cardRows}}) {
+#for my $card (@{$res->{cardRows}}) {
 
-}
+#}
